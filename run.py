8803d597c12da2e71c4057b0ae69fba19b434548
Jiwei Liu <aixueer4ever@gmail.com>
2020-12-03 00:38:15 -0800

    more cleaning

--- run.py
+++ run.py
@@ -5,16 +5,18 @@ from datetime import datetime
 import sys
 import os
 import subprocess
+
 from bayesmark.data import DATA_LOADERS, REAL_DATA_LOADERS
 from bayesmark.constants import MODEL_NAMES
 from time import sleep,time
 import glob
 from random import shuffle, randint
 import numpy as np
-from utils import get_run_name, run_cmd, run_bayesmark_init
+from combine_experiments import combine
+from utils import get_run_name, run_cmd, run_bayesmark_init, run_bayesmark_agg, run_bayesmark_anal
 
 no_multi_class_cuml = ['RF-cuml', 'SVM-cuml', 'xgb-cuml']
-multi_class_data = ['iris', 'digits', 'wine']
+multi_class_data = ['iris', 'digits', 'wine', 'mnist']
 real_data = []
 COUNTER = 0 
 
@@ -44,9 +46,7 @@ def run_all(opt, n_jobs=16, N_STEP=16, N_BATCH=8, N_REPEAT=1,
     if os.path.exists(f"{out_path}/{name}"):
         assert 0, f"{out_path}/{name} already exists"
                       
-
-    cmd = f"bayesmark-init -dir {out_path} -b {name}"
-    run_cmd(cmd)
+    run_bayesmark_init(out_path, name)
     
     tag = '-cuml-all' if run_cuml else ''
     baseline = f"{in_path}/baseline-{N_STEP}-{N_BATCH}{tag}.json"
@@ -72,7 +72,10 @@ def run_all(opt, n_jobs=16, N_STEP=16, N_BATCH=8, N_REPEAT=1,
         model_names = [i for i in model_names if i.endswith('-cuml')]# and 'MLP' not in i and 'xgb' not in i]
 
     if must_have_tag is not None:
-        model_names = [i for i in model_names if must_have_tag in i]
+        if isinstance(must_have_tag, list):
+            model_names = [i for i in model_names if isin(i, must_have_tag)]
+        else:
+            model_names = [i for i in model_names if must_have_tag in i]
     print(model_names)
 
     for data in data_loaders:
@@ -97,31 +100,34 @@ def run_all(opt, n_jobs=16, N_STEP=16, N_BATCH=8, N_REPEAT=1,
         done, n = check_complete(N, out_path, name)
         sofar = time() - start    
         print(f"{sofar:.1f} seconds passed, {N - len(cmds)} tasks launched, {n} out of {N} tasks finished ...")
-        sleep(3)
         if done:
             break
+        sleep(3)
         if last < n:
             lc = len(cmds)
             cmds = run_cmds(cmds, min(n-last, lc))
         last = n
         
-    cmd = f"bayesmark-agg -dir {out_path} -b {name}"
-    run_cmd(cmd)
-    
-    cmd = f"bayesmark-anal -dir {out_path} -b {name} -v"
-    run_cmd(cmd)
+    run_bayesmark_agg(out_path, name)
+    run_bayesmark_anal(out_path, name)
     
     duration = time() - start
     print(f"All done!! {name} Total time: {duration:.1f} seconds")
+    return name, duration
     
+def isin(i, must_have_tag):
+    for j in must_have_tag:
+        if j in i:
+            return True
+    return False
+
 def run_cmds(cmds, n):
     global COUNTER
     for _ in range(n):
         cmd = cmds.pop()
-        os.environ["CUDA_VISIBLE_DEVICES"]=str(COUNTER%4)
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(COUNTER%8)
         COUNTER += 1
         run_cmd(cmd)
-        sleep(3)
     return cmds
 
 def check_complete(N, out_path, name):
@@ -133,13 +139,16 @@ def check_complete(N, out_path, name):
     return n == N, n
 
 if __name__ == '__main__':
-    opt = './example_submissions/hyperopt'
-    opt = 'RandomSearch'
+    opt = './example_submissions/pysotopen'
 
     # sklearn dataset
-    #run_all(opt, N_STEP=16, N_BATCH=8, N_REPEAT=3, quick_check=False, n_jobs=32, run_cuml=True, must_have_tag='xgb')
+    name1,t1 = run_all(opt, N_STEP=16, N_BATCH=8, N_REPEAT=3, quick_check=False, n_jobs=32, run_cuml=True)#, must_have_tag='xgb')
 
     # real dataset
-    run_all(opt, N_STEP=16, N_BATCH=8, N_REPEAT=3, quick_check=False, n_jobs=4, run_cuml=True,
-            data_loaders=REAL_DATA_LOADERS, must_have_tag='xgb' 
+    name2,t2 = run_all(opt, N_STEP=16, N_BATCH=8, N_REPEAT=3, quick_check=False, n_jobs=32, run_cuml=True,
+            data_loaders=REAL_DATA_LOADERS, must_have_tag=['MLP', 'xgb'] 
             )
+    print(name1, name2)
+    combine([name1, name2])
+
+    print(f"Total time: {t1+t2:.1f} seconds")
--- utils.py
+++ utils.py
@@ -11,6 +11,14 @@ def run_bayesmark_init(out_path, name):
     cmd = f"bayesmark-init -dir {out_path} -b {name}"
     run_cmd(cmd)
 
+def run_bayesmark_agg(out_path, name):
+    cmd = f"bayesmark-agg -dir {out_path} -b {name}"
+    run_cmd(cmd)
+
+def run_bayesmark_anal(out_path, name):
+    cmd = f"bayesmark-anal -dir {out_path} -b {name} -v"
+    run_cmd(cmd)
+
 def run_cmd(cmd):
     print(cmd)
     os.system(cmd)
